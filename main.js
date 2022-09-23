d1a4c8782cae025d3469e80b4112d060c4f9a59e
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-09-22 21:08:19 -0400

    Add a function for computer player

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
-function getRandomSaying(arr) {
-  var randomIndex = Math.floor(Math.random()* arr.length)
-  var saying = arr[randomIndex]
-  return saying
+// document QuerySelectors
+var classicGame = document.querySelector('#classicGame');
+var superGame = document.querySelector('#superGame');
+var classicForm = document.querySelector('#classicForm');
+var gamesContainer = document.querySelector('#gamesContainer')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Event Listeners
+classicGame.addEventListener('click', oldSchool);
+
+
+
+
+
+
+
+
+
+//  functions
+function oldSchool() {
+  classicForm.classList.remove('hidden');
+  classicGame.classList.add('hidden');
+
+}
+
+function computerPlayer(gameArr) {
+  var randomIndex = Math.floor(Math.random() * gameArr.length)
+  var computerFighter = gameArr[randomIndex]
+  return computerFighter
 };
+
+
+
+
+
+
+
+
+
+//  variables
+var classicGame = ['paper','scissors','rock']
+var superGame = ['paper','scissors','rock','villian']
