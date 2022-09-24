ca434ca37ec787805e77739c9b565d2ebd0fab7d
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-09-24 11:33:55 -0400

    Add queryselectors for interations

--- main.js
+++ main.js
@@ -2,7 +2,15 @@
 var classicGameArea = document.querySelector('#classicGame');
 var injusticeGameArea = document.querySelector('#superGame');
 var hiddenForm = document.querySelector('#fighterForm');
-
+var hiddenHeroButton = document.querySelector('#hiddenHero');
+var hiddenVillainButton = document.querySelector('#hiddenVillain');
+var rockButton = document.querySelector('#rockBtn');
+var paperButton = document.querySelector('#paperBtn');
+var scissorsButton = document.querySelector('#scissorsBtn');
+var changeGameButton = document.querySelector('#gameSwitch');
+var gameAnnouncer = document.querySelector('#changingPhrase')
+var userWinsCounter = document.querySelector('#userWins');
+var computerWinsCounter = document.querySelector('#computerWins');
 
 
 
@@ -20,8 +28,13 @@ var hiddenForm = document.querySelector('#fighterForm');
 
 // Event Listeners
 classicGameArea.addEventListener('click', oldSchool);
-
-
+injusticeGameArea.addEventListener('click',royal);
+// rockButton.addEventListener('click',)
+// paperButton.addEventListener('click',)
+// scissorsButton.addEventListener('click',)
+// hiddenHeroButton.addEventListener('click',)
+// hiddenVillainButton.addEventListener('click',)
+// changeGameButton.addEventListerner('click',)
 
 
 
@@ -33,15 +46,26 @@ classicGameArea.addEventListener('click', oldSchool);
 function oldSchool() {
   hiddenForm.classList.remove('hidden');
   classicGameArea.classList.add('hidden');
+  computerPlayer(classicGameFighters);
 }
 
+function royal() {
+  oldSchool();
+  displayCharacters();
+  computerPlayer(superGameFighters);
 
+}
+function displayCharacters(){
+hiddenHeroButton.classList.remove('hidden')
+hiddenVillainButton.classList.remove('hidden')
+}
 
 function computerPlayer(gameFightersArr) {
   var randomIndex = Math.floor(Math.random() * gameFightersArr.length)
   var computerFighter = gameFightersArr[randomIndex]
   return computerFighter
 };
+
 
 
 
